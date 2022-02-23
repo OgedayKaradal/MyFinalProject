8abb0b6d6aeddc93bef32b6b42c2432f53bc9563
Ögeday <ogedaykaradal@gmail.com>
2022-02-23 23:15:54 +0300

    23 February 2022

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,5 +11,7 @@ namespace Business.Abstract
         List<Product> GetAllByUnitPrice();
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetAllByUnitPrice(decimal min, decimal max);
+        List<ProductDetailDto> GetProductDetails();
+
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,6 +52,11 @@ namespace DataAccess.Concrete.InMemory
         public List<Product> GetAllByCategory(int CategoryId)
         {
             return _products.Where(p => p.CategoryId == CategoryId).ToList();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
         }
 
         public void Update(Product product)
