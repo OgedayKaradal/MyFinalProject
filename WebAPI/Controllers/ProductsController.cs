85d6e892201e3499a8b0c512171e2cc151b4c19b
OgedayKaradal <ogedaykaradal@gmail.com>
2022-04-01 23:27:59 +0300

    1 April 2022

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+        }
+    }
+}
