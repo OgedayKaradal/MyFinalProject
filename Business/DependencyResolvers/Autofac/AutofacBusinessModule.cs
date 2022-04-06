3db4edc83da39a86e62c714f3b50101cfe74d515
OgedayKaradal <ogedaykaradal@gmail.com>
2022-04-06 14:59:01 +0300

    Create AutofacBusinessModule.cs
    6 April 2022 (2)

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
