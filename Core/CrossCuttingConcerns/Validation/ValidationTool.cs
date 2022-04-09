ec6b9fbf732fad0fee469e6d4f3fad0982a5f601
OgedayKaradal <ogedaykaradal@gmail.com>
2022-04-09 11:54:03 +0300

    9 April 2022

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validate)
+        {
+            var context = new ValidationContext<Product>(product);
+            ProductValidator productValidator = new ProductValidator();
+            var result = productValidator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
