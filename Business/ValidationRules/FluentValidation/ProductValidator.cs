963e2c836bfd8880ad6f0e6d7e10a263192904f5
Ögeday Karadal <ogedaykaradal@gmail.com>
2022-08-20 19:26:19 +0300

    Add new validation rules

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -12,6 +12,16 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p => p.ProductName).NotEmpty();
             RuleFor(p => p.UnitPrice).NotEmpty();
             RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitsInStock).NotEmpty();
+            RuleFor(p => p.UnitsInStock).Must(StockGreaterThanZero).WithMessage("Number of stock must be greater than or equal to 0.");
+            RuleFor(p => p.CategoryId).NotEmpty();
+        }
+
+        bool StockGreaterThanZero(short number)
+        {
+            if (number >= 0)
+                return true;
+            return false;
         }
     }
 }
